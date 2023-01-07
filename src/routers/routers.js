ccccd87b359c36e87c3c5c4751c216fb577ffdf7
alperalt1 <85470133+alperalt1@users.noreply.github.com>
2023-01-07 18:26:18 -0500

    Delete src directory

--- src/routers/routers.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const { ini, initStore} = require('../controllers/controllers.js');
-
-router.get('/Inicio',ini);
-router.get('/Inicio/Store',initStore);
-module.exports = router;
